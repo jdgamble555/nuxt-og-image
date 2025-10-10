981ff3d7060732dedd65156c63e157735af6c95d
Jonathan Gamble <jdgamble555@gmail.com>
2025-10-09 20:14:07 -0500

    nuxt image test

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineOgImageComponent("Nuxt", {
+  title: "Nuxt Commerce",
+  description:
+    "A high-performance, server-rendered E-commerce app built with Nuxt & Shopify",
+  theme: "#4ADE80",
+  headline: "",
+  colorMode: "dark",
+});
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
