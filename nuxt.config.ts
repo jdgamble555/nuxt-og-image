448d78d16e27bccef97685e5a43a5a7db773adbe
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-04 11:43:52 -0600

    lazy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   //modules: ['nuxt-og-image'],
   nitro: {
     preset: 'vercel-edge',
-    wasm: { esmImport: true }
+    wasm: { esmImport: true, lazy: true }
   },
 
   /*ogImage: {
