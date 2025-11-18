4bbd6c7d67820dcf6ad253b082e6f38dfa2989c7
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-17 21:26:12 -0600

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,17 @@
+// import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  //modules: ['nuxt-og-image'],
   nitro: {
     preset: 'vercel-edge',
     wasm: { esmImport: true, lazy: true },
     experimental: {
-      wasm: true 
+      wasm: true
     }
+  },
+  vite: {
+    plugins: []
   }
-  /*ogImage: {
-    compatibility: {
-      prerender: {
-        chromium: false,
-        sharp: false
-      }
-    }
-  }*/,
-
-  //modules: ['nuxt-og-image']
 })
--- server/api/og.get.ts
+++ server/api/og.get.ts
@@ -1,27 +1,25 @@
-import { ImageResponse as VercelOGImageResponse } from '@vercel/og';
-import { html } from 'satori-html';
+import { fetchFont, ImageResponse, html } from 'og-img';
+//import { html } from 'satori-html';
 
 
 const data = `<div class="flex h-full w-full flex-col items-center justify-center bg-white p-10">
 	<div class="text-center text-[60px] font-bold text-black">Welcome to My Site</div>
 	<div class="mt-5 text-[30px] text-gray-600">Generated with Nuxt ImageResponse</div>
-</div>`;
+</div>`
 
+// Use a reliable TTF font URL that works with image generation
+const fontUrl = 'https://fonts.gstatic.com/s/opensans/v40/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVc.ttf';
 
-class ImageResponse extends VercelOGImageResponse {
-    constructor(
-        data: string,
-        options?: ConstructorParameters<typeof VercelOGImageResponse>['1']
-    ) {
-        const element = html(data);
-        super(element, options);
-    }
-}
-
-
-export default defineEventHandler(() => {
-    return new ImageResponse(data, {
+export default defineEventHandler(async () => {
+    return new ImageResponse(html(data), {
         width: 1200,
-        height: 630
-    });
+        height: 630,
+        fonts: [{
+            name: 'Open Sans',
+            // Use Open Sans TTF which is well-supported for image generation
+            data: await fetchFont(fontUrl),
+            weight: 400,
+            style: 'normal',
+        }]
+    })
 })
