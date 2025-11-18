4ecf87eaf07df337176eff21a53b64b67e823898
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-17 20:53:58 -0600

    vercel test

--- app/app.vue
+++ app/app.vue
@@ -7,11 +7,21 @@ defineOgImageComponent("Nuxt", {
   headline: "",
   colorMode: "dark",
 });
+
+const num = Math.random();
 </script>
 
 <template>
   <div>
-    <a href="https://www.opengraph.xyz/url/https%3A%2F%2Fnuxt-og-image.vercel.app%2F">Open Graph Test</a>
+    <a
+      href="https://www.opengraph.xyz/url/https%3A%2F%2Fnuxt-og-image.vercel.app%2F"
+    >
+      Open Graph Test
+    </a>
+    <br />
+    <a :href="`/api/og?num=${num}`" target="_blank">
+      Show OG Image
+    </a>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
