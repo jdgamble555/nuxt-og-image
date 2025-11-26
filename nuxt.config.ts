292c980df360dbb28b4bdc3369240c9a8cd67a62
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-25 18:54:45 -0600

    c

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -7,19 +5,10 @@ export default defineNuxtConfig({
   nitro: {
     inlineDynamicImports: true,
     preset: 'vercel-edge',
-    /*wasm: { esmImport: true, lazy: true },
-    experimental: {
-      wasm: true
-    }*/
   },
   vite: {
     ssr: {
       noExternal: [/@cf-wasm\/.*/]
-    },
-    plugins: [
-      cloudflareModules({
-        target: 'edge-light'
-      })
-    ]
+    }
   }
 })
