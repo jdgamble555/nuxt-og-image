2bf7ff34ecd4a5730839e2e9f4465b8a1712c4f5
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-29 09:29:06 -0600

    vercel

--- server/api/og.get.ts
+++ server/api/og.get.ts
@@ -4,7 +4,7 @@ import { html } from 'satori-html';
 
 const data = `<div class="flex h-full w-full flex-col items-center justify-center bg-white p-10">
 	<div class="text-center text-[60px] font-bold text-black">Welcome to My Site</div>
-	<div class="mt-5 text-[30px] text-gray-600">Generated with Nuxt ImageResponse</div>
+	<div class="mt-5 text-[30px] text-gray-600">Generated with Nuxt ImageResponse and deployed to Vercel Edge</div>
 </div>`
 
 
